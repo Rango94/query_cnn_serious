e89049b925f6afeafabb51ef916e946599f88f02
wangnanzhi <wangnanzhi@baidu.com>
2018-07-05 12:35:33 +0800

    a cnn attempt

--- query_cnn_infernece.py
+++ query_cnn_infernece.py
@@ -2,56 +2,109 @@ import tensorflow as tf
 
 NUM_CHANNELS=1
 
-
+WORD_LEN=200
 FEATURE_SHAPE=[12,3665]
 
-CONV1_DEEP=32
-CONV1_SIZE=5
+CONV1_DEEP=1
+CONV1_WIDE=WORD_LEN
+CONV1_HIGH=1
 
-CONV2_DEEP=64
-CONV2_SIZE=5
+CONV2_DEEP=1
+CONV2_WIDE=WORD_LEN
+CONV2_HIGH=2
 
-FC_SIZ=512
-NUM_LABELS=10
+CONV3_DEEP=1
+CONV3_WIDE=WORD_LEN
+CONV3_HIGH=3
 
-WORD_LEN=200
+CONV4_DEEP=1
+CONV4_WIDE=WORD_LEN
+CONV4_HIGH=4
+
+FC_SIZ=24
+NUM_LABELS=21
+
+
 
 
 
 def inference(input_tensor,train,regularizer):
+    #embedding层，目前定义的输入数据shape是[batch_size,12,3665]
+    #需要将其映射为[batch_size,12,200]
+    #由于cnn的特性，我们需要将它reshape成[batch_size,12,200,1]
     with tf.variable_scope('layer0-embedding'):
-        embedding_weights=tf.get_variable('weight',[FEATURE_SHAPE[1],WORD_LEN],initializer=tf.truncated_normal_initializer(stddev=0.1))
-        embedding_baises=tf.get_variable('bais',[FEATURE_SHAPE[0],WORD_LEN],initializer=tf.constant_initializer(0.0))
-        embedding=tf.matmul(input_tensor,embedding_weights)+embedding_baises
+        input_tensor = tf.reshape(input_tensor, [-1, FEATURE_SHAPE[1]])
+        embedding_weights = tf.get_variable('weight', [FEATURE_SHAPE[1], WORD_LEN],
+                                            initializer=tf.random_uniform_initializer(maxval=0.5 / WORD_LEN,
+                                                                                      minval=-0.5 / WORD_LEN))
+        embedding_baises = tf.get_variable('bais', [WORD_LEN], initializer=tf.constant_initializer(0.0))
+        embedding = tf.matmul(input_tensor, embedding_weights) + embedding_baises
+        embedding = tf.reshape(embedding, [-1, 12, WORD_LEN, NUM_CHANNELS])
 
     with tf.variable_scope('layer1-conv1'):
-        conv1_weights=tf.get_variable('wegiht',[CONV1_SIZE,CONV1_SIZE,NUM_CHANNELS,CONV1_DEEP],initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv1_weights=tf.get_variable('wegiht',[CONV1_WIDE,CONV1_HIGH,NUM_CHANNELS,CONV1_DEEP],initializer=tf.truncated_normal_initializer(stddev=0.1))
         conv1_biases=tf.get_variable('bais',[CONV1_DEEP],initializer=tf.constant_initializer(0.0))
         conv1=tf.nn.conv2d(embedding,conv1_weights,strides=[1,1,1,1],padding='SAME')
         relu1=tf.nn.relu(tf.nn.bias_add(conv1,conv1_biases))
 
-    with tf.name_scope('layer2-pool1'):
-        pool1=tf.nn.max_pool(relu1,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
-
-
-    with tf.variable_scope('layer3-conv2'):
-        conv2_weights=tf.get_variable('weight',[CONV2_SIZE,CONV2_SIZE,NUM_CHANNELS,CONV2_DEEP],initializer=tf.truncated_normal_initializer(stddev=0.1))
-        conv2_biases=tf.get_variable('bias',[CONV2_DEEP],initializer=tf.truncated_normal_initializer(0.0))
-        conv2=tf.nn.conv2d(pool1,conv2_weights,strides=[1,1,1,1],padding='SAME')
+    with tf.variable_scope('layer1-conv2'):
+        conv2_weights=tf.get_variable('wegiht',[CONV1_WIDE,CONV1_HIGH,NUM_CHANNELS,CONV1_DEEP],initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv2_biases=tf.get_variable('bais',[CONV1_DEEP],initializer=tf.constant_initializer(0.0))
+        conv2=tf.nn.conv2d(embedding,conv2_weights,strides=[1,1,1,1],padding='SAME')
         relu2=tf.nn.relu(tf.nn.bias_add(conv2,conv2_biases))
 
-    with tf.name_scope('layer4-pool2'):
-        pool2=tf.nn.max_pool(relu2,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
+    with tf.variable_scope('layer1-conv3'):
+        conv3_weights=tf.get_variable('wegiht',[CONV1_WIDE,CONV1_HIGH,NUM_CHANNELS,CONV1_DEEP],initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv3_biases=tf.get_variable('bais',[CONV1_DEEP],initializer=tf.constant_initializer(0.0))
+        conv3=tf.nn.conv2d(embedding,conv3_weights,strides=[1,1,1,1],padding='SAME')
+        relu3=tf.nn.relu(tf.nn.bias_add(conv3,conv3_biases))
+
+    with tf.variable_scope('layer1-conv4'):
+        conv4_weights=tf.get_variable('wegiht',[CONV1_WIDE,CONV1_HIGH,NUM_CHANNELS,CONV1_DEEP],initializer=tf.truncated_normal_initializer(stddev=0.1))
+        conv4_biases=tf.get_variable('bais',[CONV1_DEEP],initializer=tf.constant_initializer(0.0))
+        conv4=tf.nn.conv2d(embedding,conv4_weights,strides=[1,1,1,1],padding='SAME')
+        relu4=tf.nn.relu(tf.nn.bias_add(conv4,conv4_biases))
+
+    with tf.name_scope('layer2-pool1'):
+        conv1_shape=relu1.get_shape().as_list()
+        pool1=tf.nn.max_pool(relu1,ksize=[1,conv1_shape[0],conv1_shape[1],1],strides=[1,2,2,1],padding='SAME')
+
+    with tf.name_scope('layer2-pool2'):
+        conv2_shape = relu1.get_shape().as_list()
+        pool2=tf.nn.max_pool(relu2,ksize=[1,conv2_shape[0],conv2_shape[1],1],strides=[1,2,2,1],padding='SAME')
+
+    with tf.name_scope('layer2-pool3'):
+        conv3_shape = relu1.get_shape().as_list()
+        pool3=tf.nn.max_pool(relu3,ksize=[1,conv3_shape[0],conv3_shape[1],1],strides=[1,2,2,1],padding='SAME')
+
+    with tf.name_scope('layer2-pool4'):
+        conv4_shape = relu1.get_shape().as_list()
+        pool4=tf.nn.max_pool(relu4,ksize=[1,conv4_shape[0],conv4_shape[1],1],strides=[1,2,2,1],padding='SAME')
 
 
-    pool_shape=pool2.get_shape().as_list()
+
+    pool_shape=pool1.get_shape().as_list()
     nodes=pool_shape[1]*pool_shape[2]*pool_shape[3]
+    reshaped1=tf.reshape(pool1,[pool_shape[0],nodes])
 
-    reshaped=tf.reshape(pool2,[pool_shape[0],nodes])
+    pool_shape = pool2.get_shape().as_list()
+    nodes = pool_shape[1] * pool_shape[2] * pool_shape[3]
+    reshaped2 = tf.reshape(pool2, [pool_shape[0], nodes])
+
+    pool_shape = pool3.get_shape().as_list()
+    nodes = pool_shape[1] * pool_shape[2] * pool_shape[3]
+    reshaped3 = tf.reshape(pool3, [pool_shape[0], nodes])
+
+    pool_shape = pool4.get_shape().as_list()
+    nodes = pool_shape[1] * pool_shape[2] * pool_shape[3]
+    reshaped4 = tf.reshape(pool4, [pool_shape[0], nodes])
+
+    reshaped=tf.concat([reshaped1,reshaped2],1)
+    reshaped=tf.concat([reshaped,reshaped3],1)
+    reshaped=tf.concat([reshaped,reshaped4],1)
 
     with tf.variable_scope('layer5,fc1'):
         fc1_weights=tf.get_variable('weight',[nodes,FC_SIZ],initializer=tf.truncated_normal_initializer(stddev=0.1))
-
         if regularizer!=None:
             tf.add_to_collection('losses',regularizer(fc1_weights))
         fc1_biases=tf.get_variable('bias',[FC_SIZ],initializer=tf.constant_initializer(0.1))
--- query_cnn_train.py
+++ query_cnn_train.py
@@ -18,8 +18,8 @@ MODEL_NAME='model'
 REGULARIZER_RATE=0.0001
 
 def train(dh):
-    x=tf.placeholder(tf.float32, [None, query_cnn_infernece.FEATURE_SHAPE, query_cnn_infernece.FEATURE_SHAPE, query_cnn_infernece.NUM_CHANNELS], name='x-input')
-    y_=tf.placeholder(tf.float32, [query_cnn_infernece.NUM_LABELS], name='y-input')
+    x=tf.placeholder(tf.float32, [None, query_cnn_infernece.FEATURE_SHAPE[0], query_cnn_infernece.FEATURE_SHAPE[1]], name='x-input')
+    y_=tf.placeholder(tf.float32, [None,query_cnn_infernece.NUM_LABELS], name='y-input')
     Regularzer=tf.contrib.layers.l2_regularizer(REGULARIZER_RATE)
     y=query_cnn_infernece.inference(x, True, Regularzer)
     global_step=tf.Variable(0,trainable=False)
@@ -27,7 +27,7 @@ def train(dh):
     variable_averages=tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY,global_step)
     variable_averages_op=variable_averages.apply(tf.trainable_variables())
 
-    cross_entropy=tf.nn.softmax_cross_entropy_with_logits_v2(logits=y,labels=tf.argmax(y_,1))
+    cross_entropy=tf.nn.softmax_cross_entropy_with_logits_v2(logits=y,labels=y_)
     cross_entropy_mean=tf.reduce_mean(cross_entropy)
     loss=cross_entropy_mean+tf.add_n(tf.get_collection('losses'))
     learning_rate=tf.train.exponential_decay(LEARNING_RATE_BASE,global_step,dh.train_file_num*5000/BATCH_SIZE,LEARNING_RATE_DECAY)
@@ -36,7 +36,7 @@ def train(dh):
     with tf.control_dependencies([train_step,variable_averages_op]):
         train_op=tf.no_op(name='train')
 
-    saver=tf.train.Saver()
+    # saver=tf.train.Saver()
 
     TRAINING_STEPS = 30000
 
@@ -46,12 +46,20 @@ def train(dh):
         tf.initialize_all_variables().run()
         for i in range(TRAINING_STEPS):
             xs,ys=dh.next_batch()
-            xs=tf.reshape(xs, [BATCH_SIZE, query_cnn_infernece.FEATURE_SHAPE, query_cnn_infernece.FEATURE_SHAPE, query_cnn_infernece.NUM_CHANNELS])
+            # print(sess.run(y,feed_dict={x:xs,y_:ys}))
             _,loss_value,step=sess.run([train_op,loss,global_step],feed_dict={x:xs,y_:ys})
-            if i%1000==0:
+            if i%10==0:
                 print('After %d training steps, loss on training batch is %g.' %(step,loss_value))
-                saver.save(sess,os.path.join(MODEL_SAVE_PATH,MODEL_NAME),global_step=global_step)
-
+                # saver.save(sess,os.path.join(MODEL_SAVE_PATH,MODEL_NAME),global_step=global_step)
+                y_out=sess.run(y,feed_dict={x:xs,y_:ys}).tolist()
+                y_out_r=ys.tolist()
+                r=0
+                tt=0
+                for idx,i in enumerate(y_out):
+                    if i.index(max(i))==y_out_r[idx].index(max(y_out_r[idx])):
+                        r+=1
+                    tt+=1
+                print(r/tt)
 def main(argv=None):
     dh=data_helper()
     train(dh)
